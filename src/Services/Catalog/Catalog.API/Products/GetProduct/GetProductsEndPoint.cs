d06eaf259547f1cf8812f3e9356b0268d82f2ace
alhadi <alhadi@DESKTOP-T53A4AG>
2025-03-09 00:53:49 +0600

    GetProductById

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndPoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Catalog.API.Products.GetProduct
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProduct
 {
-    public class GetProductsEndPoint
+    public record GetProductResponse(IEnumerable<Product> Products);
+    public class GetProductsEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductQuery());
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProduct")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product")
+            .WithDescription("Get Product");
+        }
     }
 }
